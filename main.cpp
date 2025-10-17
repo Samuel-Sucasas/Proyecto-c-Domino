a8cd8e51636150b5e67f6b7dd52c3f8ac24233b8
Samuel-Sucasas <samuelsucasas08@gmail.com>
2025-10-17 15:50:29 -0400

    *Se agregaron pocas cosas pero un mini avance -Domitivas: Se agrego la libreria,se creo el struct con sus dos numeros enteros y fue añadido en el main -Main: Se hizo el include de Domitivas y se agrego unos couts

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <Iostream>
+#include "Domitivas.h"
 using namespace std;
 
 int main(){
-
+    int players=0;
+    cout<<"Bienvenido al Domino doble 6"<<endl;
+    cout<<"Cuantos jugadores Desean jugar: "<<endl;cin>>players;
     return 0;
 }
