dbb69be83c2dad7c9def691b4158c9bc66414202
Samuel-Sucasas <samuelsucasas08@gmail.com>
2025-11-08 19:51:06 -0400

    *Se concluyo Dominitivas -se cambiaron unos nombres de las funciones. -se agrego una funcion para eliminar fichas. -se añadio un contador de fichas.

--- Domitivas.h
+++ Domitivas.h
@@ -1,4 +1,6 @@
-#include <Iostream>
+#include <iostream>
+using namespace std;
+
 struct Ficha{
     int Dato1;
     int Dato2;
@@ -17,40 +19,76 @@ bool FichaVacia(Ficha *inicio){
     return inicio==NULL;
 }
 
-void mostrarFichas(Ficha *inicio){
-    Ficha *mover;
+void mostrarFichaEnlinea(const Ficha *f){
+    if (!f) return;
+    // Imprimo con anchura aproximada
+    cout<<"[";
+    if (f->Dato1 < 10) cout<<' ';
+    cout<<f->Dato1<<"|";
+    if (f->Dato2 < 10) cout<<' ';
+    cout<<f->Dato2<<"]";
+}
 
-    if (!FichaVacia(inicio))
-    {
-        mover = inicio;
-        while (mover != NULL)
-        {
-            std::cout<<"|"<<mover->Dato1<<"|"<<mover->Dato2;
+void mostrarFichas(Ficha *inicio){
+    Ficha *mover = inicio;
+    if (!FichaVacia(inicio)){
+        while (mover != NULL){
+            mostrarFichaEnlinea(mover);
+            cout<<" ";
             mover = mover->prox;
         }
+        cout<<endl;
+    } else {
+        cout<<"[vacío]"<<endl;
     }
-    else
-    std::cout<<"El jugador no tiene fichas";
 }
 
 void insertarUltimoTabla(Ficha **inicio, int valor1,int valor2){
     Ficha *nuevo = CrearFicha(valor1,valor2);
- 
-   if (FichaVacia(*inicio)) {
-       *inicio = nuevo;
-    } 
-    else {
-       Ficha *auxiliar = *inicio;
-       while (auxiliar->prox != NULL) {
-           auxiliar = auxiliar->prox;
-       }
+    if (FichaVacia(*inicio)){
+        *inicio = nuevo;
+    } else {
+        Ficha *auxiliar = *inicio;
+        while (auxiliar->prox != NULL) auxiliar = auxiliar->prox;
         auxiliar->prox = nuevo;
-  }
-   
-} 
+    }
+}
 
 void insertarPrimeroTabla(Ficha **inicio, int valor1,int valor2) {
-  Ficha *nuevo = CrearFicha(valor1,valor2);
-  nuevo->prox = *inicio;
-  *inicio = nuevo;
+    Ficha *nuevo = CrearFicha(valor1,valor2);
+    nuevo->prox = *inicio;
+    *inicio = nuevo;
+}
+
+int contarFichas(Ficha *inicio){
+    int cont = 0;
+    Ficha *aux = inicio;
+    while(aux){
+        cont++;
+        aux = aux->prox;
+    }
+    return cont;
+}
+
+// Elimina y devuelve la ficha en el índice (0-based). Retorna true si eliminó.
+bool eliminarFichaPorIndice(Ficha **inicio, int indice, int &salida1, int &salida2){
+    if (FichaVacia(*inicio)) return false;
+    Ficha *actual = *inicio;
+    Ficha *anterior = NULL;
+    int i = 0;
+    while(actual && i < indice){
+        anterior = actual;
+        actual = actual->prox;
+        i++;
+    }
+    if (!actual) return false;
+    salida1 = actual->Dato1;
+    salida2 = actual->Dato2;
+    if (anterior == NULL){
+        *inicio = actual->prox;
+    } else {
+        anterior->prox = actual->prox;
+    }
+    delete actual;
+    return true;
 }
