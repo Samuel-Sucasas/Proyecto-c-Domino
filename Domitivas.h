3b393242925558fb89742069887a5276bc590f2c
Samuel-Sucasas <samuelsucasas08@gmail.com>
2025-11-08 20:57:28 -0400

    *Se agregaron nuevas primitivas y dos struct -Se agrego una estructura para jugadores.  -Otro de Nodos enteros.  - se agregaron sus primitivas.

--- Domitivas.h
+++ Domitivas.h
@@ -1,10 +1,22 @@
 #include <iostream>
 using namespace std;
-//Primitivas para las Fichas de domino
+//Primitivas para las Fichas de domino y los judadores
 struct Ficha{
     int Dato1;
     int Dato2;
     Ficha *prox;
+};
+
+// Estructuras auxiliares: Jugador (lista de jugadores) y NodoInt (lista int)
+struct Jugador {
+    int id;        // id 0..N-1
+    Ficha *mano;
+    Jugador *prox;
+};
+
+struct NodoInt {
+    int val;
+    NodoInt *prox;
 };
 
 Ficha* CrearFicha(int Valor1,int Valor2){
@@ -91,4 +103,89 @@ bool eliminarFichaPorIndice(Ficha **inicio, int indice, int &salida1, int &salid
     }
     delete actual;
     return true; //Retorna true si eliminó.
+}
+
+// Crear lista de jugadores (ids 0..jugadoresCount-1)
+void crearJugadores(Jugador **cabeza, int jugadoresCount){
+    *cabeza = NULL;
+    Jugador *ultimo = NULL;
+    for(int i=0;i<jugadoresCount;i++){
+        Jugador *j = new Jugador;
+        j->id = i;
+        j->mano = NULL;
+        j->prox = NULL;
+        if (*cabeza == NULL) {
+            *cabeza = j;
+            ultimo = j;
+        } else {
+            ultimo->prox = j;
+            ultimo = j;
+        }
+    }
+}
+
+Jugador* encontrarJugador(Jugador *cabeza, int id){
+    Jugador *aux = cabeza;
+    while(aux){
+        if (aux->id == id) return aux;
+        aux = aux->prox;
+    }
+    return NULL;
+}
+
+int contarJugadores(Jugador *cabeza){
+    int c = 0;
+    Jugador *aux = cabeza;
+    while(aux){ c++; aux = aux->prox; }
+    return c;
+}
+
+// ------------------------------------------------------------------
+// Operaciones NodoInt (listas de enteros)
+// ------------------------------------------------------------------
+void appendNodoInt(NodoInt **cabeza, int v){
+    NodoInt *n = new NodoInt;
+    n->val = v;
+    n->prox = NULL;
+    if (*cabeza == NULL) { *cabeza = n; return; }
+    NodoInt *aux = *cabeza;
+    while(aux->prox) aux = aux->prox;
+    aux->prox = n;
+}
+
+int contarNodoInt(NodoInt *cabeza){
+    int c = 0;
+    NodoInt *aux = cabeza;
+    while(aux){ 
+        c++; aux = aux->prox;
+    }
+    return c;
+}
+
+// Elimina y devuelve el valor del nodo en índice  de una lista de int
+bool eliminarNodoIntPorIndice(NodoInt **cabeza, int indice, int &salidaVal){
+    if (*cabeza == NULL) return false;
+    NodoInt *actual = *cabeza;
+    NodoInt *anterior = NULL;
+    int i = 0;
+    while(actual && i < indice){
+        anterior = actual;
+        actual = actual->prox;
+        i++;
+    }
+    if (!actual) return false;
+    salidaVal = actual->val;
+    if (anterior == NULL) *cabeza = actual->prox;
+    else anterior->prox = actual->prox;
+    delete actual;
+    return true;
+}
+
+bool nodoIntContiene(NodoInt *cabeza, int v){
+    NodoInt *aux = cabeza;
+    while(aux){
+        if (aux->val == v) return true;
+        aux = aux->prox;
+    }
+    return false;
 }
